95c1ebb3b92365c9d060de5130b75952bcc40b01
szoszi <szoszig@gmail.com>
2023-07-04 10:55:50 +0200

    Initial Commit

---
+++ CryptoWork/CryptoWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWorkApp.swift
+//  CryptoWork
+//
+//  Created by Gergely Szoke on 2023. 07. 04..
+//
+
+import SwiftUI
+
+@main
+struct CryptoWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
